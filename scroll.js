382216905ab5063f27b909fe2db33264177b6356
maen <maenkh@yahoo.com>
2022-08-26 23:32:44 +0300

    video scroll

---
+++ scroll.js
@@ -0,0 +1,35 @@
+
+$('.responsive').slick({
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+  infinite: false,
+  speed: 300,
+  slidesToShow: 8,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  
+  ]
+  });
